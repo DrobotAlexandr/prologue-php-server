0a0c87eadd837556812cef5656dcc8bacf2906c4
Alexandr <82879664+DrobotAlexandr@users.noreply.github.com>
2021-07-07 18:28:36 +0300

    Update RunTime.php

--- src/RunTime/RunTime.php
+++ src/RunTime/RunTime.php
@@ -8,6 +8,10 @@ Class RunTime
     public static function runMethod($params)
     {
 
+        $request = self::runMethod__getRequest();
+
+        $response = self::runMethod__getRequest__setResponse($request, $response);
+
         $result = call_user_func_array(
             [
                 $params['className'],
@@ -20,6 +24,32 @@ Class RunTime
         );
 
         return $result;
+    }
+
+    private static function runMethod__getRequest()
+    {
+
+        $request = file_get_contents('php://input');
+
+        if (!$request) {
+            return false;
+        }
+
+        return json_decode($request);
+
+    }
+
+    private static function runMethod__getRequest__setResponse($request, $response)
+    {
+        if ($request->requestType == 'loadPage') {
+            $response['metaData'] = [
+                'title' => '',
+                'description' => '',
+                'h1' => '',
+            ];
+        }
+
+        return $response;
     }
 
 }
